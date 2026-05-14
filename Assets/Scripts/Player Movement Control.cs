caeb74458394ed80c03ad757ede6d6168ac30321
UsernameJJJ0 <chickenburgerfilms@gmail.com>
2026-05-14 18:01:09 -0400

    update 2

--- Assets/Scripts/Player Movement Control.cs
+++ Assets/Scripts/Player Movement Control.cs
@@ -14,9 +14,78 @@ public class Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //MOVEMENT
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            //left
+        }
 
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //right
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //look up for up dash
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //look down for down dash/smaller character
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            //jump
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            //dash (according to arrow controls, diagonal when up and direction pressed together)
         }
     }
 }
+
+//Dash reset on collision with berry/gem/balloon
+
+/* (GPT)
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement = Vector2.zero;
+
+        // Arrow Keys
+        if (Input.GetKey(KeyCode.LeftArrow))
+            movement.x = -1;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            movement.x = 1;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            movement.y = 1;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            movement.y = -1;
+
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * moveSpeed;
+    }
+}*/
