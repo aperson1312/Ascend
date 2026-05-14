1d2c6102e7198b97fb882f14e8e293b77a53c6c2
kirai-anon <anonymoussawyerofficial@gmail.com>
2026-05-14 17:14:46 -0400

    update~~~~~~~~~~~

---
+++ Assets/Scripts/Player Movement Control.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+    }
+}
