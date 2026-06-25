5ae53338cbc016512c2d27afabb2f39bd3c07990
aperson1312 <smcesps.chloeleung@gmail.com>
2026-06-25 17:57:56 -0400

    6

--- Assets/Scripts/Start Game.cs
+++ Assets/Scripts/Start Game.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
@@ -11,6 +12,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.X) && Input.GetKeyDown(KeyCode.C))
+        {
+            
+        }
     }
 }
